df9a5ff013fa27d66f4e11e369e9d68a837c4f40
Ahmed <ackhmedh@gmail.com>
2020-04-26 19:12:48 -0400

    first commit

---
+++ JokesAndroid/src/main/java/com/example/jokesandroid/JokesActivity.java
@@ -0,0 +1,21 @@
+package com.example.jokesandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+
+        TextView jokeTV = findViewById(R.id.JokeTextview);
+
+
+
+    }
+}
